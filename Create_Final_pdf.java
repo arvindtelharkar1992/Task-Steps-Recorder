e08fa32bdadf9a4075c2b03f23d31880c537bae7
arvind telharkar <arvindtelharkar@gmail.com>
2015-08-18 16:23:19 -0400

    Update Create_Final_pdf.java

--- Create_Final_pdf.java
+++ Create_Final_pdf.java
@@ -16,51 +16,36 @@ import com.lowagie.text.HeaderFooter;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfWriter;
-
 import java.awt.Color;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Arrays;
 import java.util.Comparator;
-
 import javax.swing.JOptionPane;
 import org.apache.poi.ss.usermodel.Footer;
-
-
-
 
 public class Create_Final_pdf {
     
     public static File[] images_for_pdf;
-    
     public static int image_count_for_pdf;
-    
-    
-    public void sort(File[] files)
-     {
-         
-          Arrays.sort(files, new Comparator(){
-@Override
-public int compare(Object o1, Object o2) {
-
-if (((File)o1).lastModified() < ((File)o2).lastModified()) 
- {
-return -1;
- }
-else if (((File)o1).lastModified() > ((File)o2).lastModified()) 
- {
-return 1;
- } 
- else 
- {
- return 0;
- }
-}
-}); 
+    public void sort(File[] files) {
+      Arrays.sort(files, new Comparator(){
+      @Override
+        public int compare(Object o1, Object o2) {
+               if (((File)o1).lastModified() < ((File)o2).lastModified()) {
+		return -1;
+ 		}
+               else if (((File)o1).lastModified() > ((File)o2).lastModified()) {
+		return 1;
+ 		}  
+ 	       else{
+ 		return 0;
+ 	       }
+         }
+        }); 
      }
     
-    public void process_images_for_pdf(String task,String workspace)
-    {
+    public void process_images_for_pdf(String task,String workspace){
         int i;
         File directory = new File(workspace+"\\"+task);
         File[] fList = directory.listFiles();
@@ -89,80 +74,16 @@ return 1;
         sort(images_for_pdf);    
         
     }
-    
-    
-    
-    
-    
-    
-    
+  
 public void create_pdf(String task,String workspace) throws Exception
 	{
-                
-	
 		Document document = new Document();
 		PdfWriter.getInstance(document,
                         new FileOutputStream(workspace+"\\"+task+"\\"+task+".pdf"));
-		
-               // Paragraph p = new Paragraph();
-               
-               //HeaderFooter hf = new HeaderFooter(new Phrase(task), true);
-               //hf.setBackgroundColor(Color.yellow);
-              
-                //document.setFooter(hf);
-               
 		document.open();
 		
-               
-                
-		/*for(i=0;i<index;i++)
-		{
-		
-             String str="sc";
-             String s=str+i;
-             r=i+1;
-		com.lowagie.text.Image image1 = 
-				com.lowagie.text.Image.getInstance(workspace+"\\"+task+"\\"+s+".jpg");
-	
-		  image1.setCompressionLevel(0);
-		      
-		 if (image1.getHeight() > image1.getWidth())
-		    {
-		        //Maximum height is 800 pixels.
-		        float percentage = 0.0f;
-		        percentage = 700 / image1.getHeight();
-		        image1.scalePercent(percentage * 100);
-		        
-		    }
-		    else
-		    {
-		        //Maximum width is 600 pixels.
-		        float percentage = 0.0f;
-		        percentage = 540 / image1.getWidth();
-		        image1.scalePercent(percentage * 100);
-		    }
-		
-		 Chunk chunk = new Chunk(r+"."+ann[i]);
-			Font font = new Font(Font.BOLD);
-                        font.setFamily("Times New Roman");
-			font.setStyle(Font.UNDERLINE);
-			font.setStyle(Font.ITALIC);
-			chunk.setFont(font);
-			chunk.setBackground(Color.GREEN);
-			document.add(chunk);
-                  
-                       
-		 document.add(image1);
-		document.newPage();
-		
-		}*/
-                
-                //File directory = new File(workspace+"\\"+task);
- //File[] fList = directory.listFiles();
- 
         for (File file : images_for_pdf)
         {
-
                 com.lowagie.text.Image image1 = 
 				com.lowagie.text.Image.getInstance(workspace+"\\"+task+"\\"+file.getName());
                 
@@ -184,10 +105,7 @@ public void create_pdf(String task,String workspace) throws Exception
 		        image1.scalePercent(percentage * 100);
 		    }
                
-                 //// document.setPageSize(image1);
-                 
                   document.add(image1);
-                 
 		  document.newPage();
                 
 	}
