e61a40327781d679d79a037085c53595620363f5
arvind telharkar <arvindtelharkar@gmail.com>
2015-08-18 16:29:37 -0400

    Update create_zip_folder.java

--- create_zip_folder.java
+++ create_zip_folder.java
@@ -19,7 +19,6 @@ import java.util.zip.ZipOutputStream;
 public class create_zip_folder {
     
     public String path_of_zipfile;
-    
     public String name_of_zipfolder;
     
     create_zip_folder(String s1,String s2)
@@ -36,27 +35,17 @@ public class create_zip_folder {
             //creating the byte array
             byte []b;
             b=new byte[1024];
-            
-           
-            
             //zip File test.zip
             File f=new File(path_of_zipfile);
             String zipFile=path_of_zipfile+"\\"+name_of_zipfolder;
             fout = new FileOutputStream(zipFile);
-            
             zout=new ZipOutputStream(new BufferedOutputStream(fout));
-            
             File []s=f.listFiles();
-            
             //Iterating over the Files in the Folder
             for(int i=0;i<s.length;i++)
             {
-                 
                 if(s[i].getName().endsWith("jpg")||s[i].getName().endsWith("pdf") || s[i].getName().endsWith("wav") || s[i].getName().endsWith("txt") )
-            {
-               
-                //System.out.println(s[i].getName());
-                
+              {
                 b=new byte[(int)s[i].length()];
                 FileInputStream fin=new FileInputStream(s[i]);
                 zout.putNextEntry(new ZipEntry(s[i].getName()));
@@ -67,21 +56,13 @@ public class create_zip_folder {
                 }
                 zout.closeEntry();
                 fin.close();
-            }
+              }
             }//end for
-            
             zout.close();
-            
         }
         catch(Exception e) 
         {    
             System.out.println("could not create Zip File");
         }
-        
-        
     }//end create_zip_file
-    
-    
-    
-    
 }
